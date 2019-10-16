ddf16368ae9100a3c1d1dacf3fe8c1e66e60fc69
Dao <mdnguyen928@gmail.com>
2019-10-16 17:25:23 +0700

    8-Cac chuc nang cua chuyen muc

--- newspage/admin/js/form.js
+++ newspage/admin/js/form.js
@@ -37,3 +37,123 @@ $('#formSignin button').on('click', function() {
         });
     }
 }); 
+
+// Tự động tạo slug
+function ChangeToSlug()
+{
+    var title, slug;
+    title = $('.title').val();
+    slug = title.toLowerCase();
+  
+    slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
+    slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
+    slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
+    slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
+    slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
+    slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
+    slug = slug.replace(/đ/gi, 'd');
+    slug = slug.replace(/\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\>|\<|\'|\"|\:|\;|_/gi, '');
+    slug = slug.replace(/ /gi, "-");
+    slug = slug.replace(/\-\-\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-/gi, '-');
+    slug = '@' + slug + '@';
+    slug = slug.replace(/\@\-|\-\@|\@/gi, '');
+    $('.slug').val(slug);
+}
+ 
+$('.slug').on('click', function() {
+    ChangeToSlug();
+});
+
+// Tải chuyên mục cha ở chức năng thêm chuyên mục
+$('#formAddCate input[type="radio"]').on('click', function() {
+    if ($('#formAddCate .type-add-cate-1:checked').prop("checked") == true) 
+    {
+        $('.parent-add-cate').addClass('hidden');
+        $('.parent-add-cate select').html('');
+    }
+    else if ($('#formAddCate .type-add-cate-2:checked').prop("checked") == true) 
+    {
+        $type_add_cate = $('#formAddCate .type-add-cate-2').val();
+ 
+        $.ajax({
+            type : 'POST',
+            url : $_DOMAIN + 'categories.php',
+            data : {
+                action : 'load_add_parent_cate',
+                type_add_cate : $type_add_cate
+            }, success : function(data) {
+                $('.parent-add-cate').removeClass('hidden');
+                $('.parent-add-cate select').html(data);
+            }, error : function() {
+                $('.parent-add-cate').removeClass('hidden');
+                $('.parent-add-cate').html('Đã có lỗi xảy ra, hãy thử lại sau.');
+            }
+        });
+    } 
+    else if ($('#formAddCate .type-add-cate-3:checked').prop("checked") == true) 
+    {
+        $type_add_cate = $('#formAddCate .type-add-cate-3').val();
+        $.ajax({
+            type : 'POST',
+            url : $_DOMAIN + 'categories.php',
+            data : {
+                action : 'load_add_parent_cate',
+                type_add_cate : $type_add_cate
+            }, success : function(data) {
+                $('.parent-add-cate').removeClass('hidden');
+                $('.parent-add-cate select').html(data);
+            }, error : function() {
+                $('.parent-add-cate').removeClass('hidden');
+                $('.parent-add-cate').html('Đã có lỗi xảy ra, hãy thử lại sau.');
+            }
+        });
+    }
+});
+
+// Thêm chuyên mục
+$('#formAddCate button').on('click', function() {
+    $this = $('#formAddCate button');
+    $this.html('Đang tải ...');
+ 
+    // Gán các giá trị trong các biến
+    $label_add_cate = $('#formAddCate #label_add_cate').val();
+    $url_add_cate = $('#formAddCate #url_add_cate').val();
+    $type_add_cate = $('#formAddCate input[name="type_add_cate"]:radio:checked').val();
+    $parent_add_cate = $('#formAddCate #parent_add_cate').val();
+    $sort_add_cate = $('#formAddCate #sort_add_cate').val();
+ 
+    // Nếu các giá trị rỗng
+    if ($label_add_cate == '' || $url_add_cate == '' || $type_add_cate == '' || $sort_add_cate == '')
+    {
+        $('#formAddCate .alert').removeClass('hidden');
+        $('#formAddCate .alert').html('Vui lòng điền đầy đủ thông tin.');
+        $this.html('Tạo');
+    }
+    // Ngược lại
+    else
+    {
+        $.ajax({
+            url : $_DOMAIN + 'categories.php',
+            type : 'POST',
+            data : {
+                label_add_cate : $label_add_cate,
+                url_add_cate : $url_add_cate,
+                type_add_cate : $type_add_cate,
+                parent_add_cate : $parent_add_cate,
+                sort_add_cate : $sort_add_cate,
+                action : 'add_cate'
+            }, success : function(data) {
+                $('#formAddCate .alert').removeClass('hidden');
+                $('#formAddCate .alert').html(data);
+                $this.html('Tạo');
+            }, error : function() {
+                $('#formAddCate .alert').removeClass('hidden');
+                $('#formAddCate .alert').html('Không thể tạo chuyên mục vào lúc này, hãy thử lại sau.');
+                $this.html('Tạo');
+            }
+        });
+    }
+});
